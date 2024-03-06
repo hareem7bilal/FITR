6540ad8c1de31e41a36c33de9d15482f76d2d8d4
Hareem Bilal <91612899+hareem7bilal@users.noreply.github.com>
2024-03-06 21:37:22 +0500

    firebase config

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/color_extension.dart';
-import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_application_1/views/login/signup_view.dart';
 
 void main() async {
- WidgetsFlutterBinding.ensureInitialized();
- SystemChrome.setPreferredOrientations([
-   DeviceOrientation.portraitUp,
-   DeviceOrientation.portraitDown,
- ]);
- //await Firebase.initializeApp();
- runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure plugin services are initialized
+  await Firebase.initializeApp(); // Initialize Firebase
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Flutter Fitness Tracker',
       debugShowCheckedModeBanner: false,
