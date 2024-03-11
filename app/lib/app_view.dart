0a0cea85b331521c94978776a2266ad8bf3e6100
Hareem Bilal <91612899+hareem7bilal@users.noreply.github.com>
2024-03-11 05:28:14 +0500

    changing images

--- app/lib/app_view.dart
+++ app/lib/app_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/blocs/auth_bloc/auth_bloc.dart';
 import 'package:flutter_application_1/utils/color_extension.dart';
-import 'package:flutter_application_1/views/onboarding/onboarding_view.dart';
+import 'package:flutter_application_1/views/onboarding/starting_view.dart';
 //import 'package:flutter_application_1/views/onboarding/onboarding_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,9 +21,9 @@ class AppView extends StatelessWidget {
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return const OnBoardingView();
+            return const StartingView();
           } else {
-            return const OnBoardingView();
+            return const StartingView();
           }
         },
       ),
